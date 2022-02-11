d94fa89f49ea885412a48bbfe786678a07d89203
seryiogonzalez <sergio.gonzalez.jimenez@gmail.com>
2022-02-11 13:47:30 +0100

    new images

--- 2_sensehat_message_display.py
+++ 2_sensehat_message_display.py
@@ -1,7 +1,8 @@
+import sys
 import time
 from sense_hat import SenseHat
 
-message_to_display = "message"
+message_to_display = sys.argv[1]
 
 sense = SenseHat()
 sense.show_message(message_to_display)
